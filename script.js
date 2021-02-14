47ff0c29fa33308075cd8c2c8d5585511a4b6845
Артём <matt-choco@yandex.ru>
2021-02-14 13:20:16 +0300

    HomeWork edited

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //1
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
 //2
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -11,13 +11,14 @@ let personalMovieDB = {
     private: false
 };
 //3
-let questionOne = prompt("Один из последних просмотренных фильмов?","");
-let questionTwo = prompt("На сколько оцените его?","");
-let filmName = questionOne;
+ const questionOne = prompt("Один из последних просмотренных фильмов?",""),
+       questionTwo = prompt("На сколько оцените его?",""),
+       questionThree = prompt("Один из последних просмотренных фильмов?",""),
+       questionFour = prompt("На сколько оцените его?","");
+    
 
-personalMovieDB.movies = {
-    filmName: questionTwo
-};
+personalMovieDB.movies[questionOne] = questionTwo
+personalMovieDB.movies[questionThree] = questionFour
 
 console.log(numberOfFilms);
 console.log(personalMovieDB);
